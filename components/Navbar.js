8aa2ae5903d7b3d391d1d556d1295e8b42321442
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-11-09 09:57:03 -0300

    fix: added link to home to francamaradev

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = React.useState(false)
@@ -7,28 +8,30 @@ function Navbar() {
       <div className='container mx-auto px-6 py-4'>
         <div className='flex items-center justify-between'>
           <div>
-            <a
-              className='flex items-center text-gray-800 hover:text-indigo-600'
-              href='#'
-            >
-              <svg
-                className='h-6 w-6 sm:h-6 sm:w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
+            <Link href='/'>
+              <a
+                className='flex items-center text-gray-800 hover:text-indigo-600'
+                href='#'
               >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
-                />
-              </svg>
+                <svg
+                  className='h-6 w-6 sm:h-6 sm:w-6'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  stroke='currentColor'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
+                  />
+                </svg>
 
-              <span className='mx-3 font-medium text-sm md:text-base'>
-                Francamara.dev
-              </span>
-            </a>
+                <span className='mx-3 font-medium text-sm md:text-base'>
+                  Francamara.dev
+                </span>
+              </a>
+            </Link>
           </div>
           <div className='flex items-center -mx-2'>
             {/* Linkedin Icon */}
