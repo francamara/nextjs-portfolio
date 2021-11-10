08bc8dc94656f39009793f5e7c911b360baa5b43
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-11-10 10:54:34 -0300

    feat: added custom page name

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <head>
+        <title>Francamaradev</title>
+      </head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
